8a336fa70da10ae675f6d82784efbd05f4d590aa
hamadou diallo <hamadou_diallo@yahoo.com>
2019-06-18 15:00:48 -0400

    Updates

---
+++ precache-manifest.0a1646ea79acc01d13a7332ea1669e08.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a48e99394a990c8984a41b9d980be8bc",
+    "url": "/pwapokedex/index.html"
+  },
+  {
+    "revision": "e01c2bed675d9cea7a73",
+    "url": "/pwapokedex/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "b687ab258c984d743a27",
+    "url": "/pwapokedex/static/css/main.1249f700.chunk.css"
+  },
+  {
+    "revision": "e01c2bed675d9cea7a73",
+    "url": "/pwapokedex/static/js/2.bf98bc90.chunk.js"
+  },
+  {
+    "revision": "c45a0e717deee9f23945",
+    "url": "/pwapokedex/static/js/3.7560870b.chunk.js"
+  },
+  {
+    "revision": "b687ab258c984d743a27",
+    "url": "/pwapokedex/static/js/main.653024dc.chunk.js"
+  },
+  {
+    "revision": "33c22a35954d256de653",
+    "url": "/pwapokedex/static/js/runtime~main.a8f97d9f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/pwapokedex/static/media/SEGA.12a3db8b.woff"
+  }
+]);
